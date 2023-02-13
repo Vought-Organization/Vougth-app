d9bd283ecc240cc4520eb8f348e87d91859cd278
guilherme.muhoz <guipe7@hotmail.com>
2023-02-13 00:16:09 -0300

    Esqueleto do projeto

---
+++ app/src/main/java/com/example/vought/login/LoginActivity.kt
@@ -0,0 +1,53 @@
+package com.example.vought.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
+import com.example.vought.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+class LoginActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_login)
+
+    val editTextEmail = findViewById<TextInputEditText>(R.id.login_edit_email)
+    val editTextPassword = findViewById<TextInputEditText>(R.id.login_edit_password)
+
+    editTextEmail.addTextChangedListener(watcher)
+    editTextPassword.addTextChangedListener(watcher)
+
+    val buttonEnter = findViewById<LoadingButton>(R.id.login_btn_enter)
+    buttonEnter.setOnClickListener {
+      buttonEnter.showProgress(true)
+
+      findViewById<TextInputLayout>(R.id.login_edit_email_input)
+        .error = "Esse e-mail é inválido"
+
+      findViewById<TextInputLayout>(R.id.login_edit_password_input)
+        .error = "Senha Incorreta"
+
+      Handler(Looper.getMainLooper()).postDelayed({
+        buttonEnter.showProgress(false)
+      }, 2000)
+    }
+  }
+
+  private val watcher = object : TextWatcher {
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+      findViewById<LoadingButton>(R.id.login_btn_enter).isEnabled = s.toString().isNotEmpty()
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+    }
+
+  }
+
+}
---
+++ app/src/main/java/com/example/vought/register/RegisterEmailFragment.kt
@@ -0,0 +1,16 @@
+package com.example.vought.register
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.vought.R
+
+class RegisterEmailFragment : Fragment() {
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    return inflater.inflate(R.layout.fragment_register_email, container, false)
+  }
+
+}
