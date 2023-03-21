23fb717183e6ede1212f3dc686cb0569cae9987f
guizin9 <guilherme.novais@bandtec.com.br>
2023-03-21 12:49:03 -0300

    segunda tela de cadastro

--- app/src/main/java/com/example/vought/register/RegisterNamePasswordFragment.kt
+++ app/src/main/java/com/example/vought/register/RegisterNamePasswordFragment.kt
@@ -10,7 +10,7 @@ import com.example.vought.R
 class RegisterNamePasswordFragment : Fragment() {
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-    return inflater.inflate(R.layout.fragment_register_name_password, container, false)
+    return inflater.inflate(R.layout.fragment_register_password, container, false)
   }
 
 }
