20e6472a07d79dfb6e6cabecb5da27d3ebbd4742
guizin9 <guilherme.novais@bandtec.com.br>
2023-04-12 19:58:54 -0300

    colocando funcionalidade na toolbar

--- app/src/main/java/com/example/vought/register/RegisterPasswordFragment.kt
+++ app/src/main/java/com/example/vought/register/RegisterPasswordFragment.kt
@@ -1,26 +1,27 @@
 package com.example.vought.register
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.vought.R
-import com.example.vought.databinding.FragmentRegisterBinding
 import com.example.vought.databinding.FragmentRegisterPasswordBinding
-import com.example.vought.databinding.FragmentRegisterWelcomeBinding
-import com.example.vought.login.LoginActivity
-
 class RegisterPasswordFragment : Fragment(R.layout.fragment_register_password) {
+
   private lateinit var binding: FragmentRegisterPasswordBinding
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     binding = FragmentRegisterPasswordBinding.bind(view)
+    setupViews()
 
+  }
+  private fun setupViews(){
     binding.apply {
+      toolbarPassword.setNavigationOnClickListener { findNavController().popBackStack()}
       loginBtnEnter.setOnClickListener {
         findNavController().navigate(R.id.action_fragment_register_password_vought_to_fragmen_welcome)
       }
     }
   }
+
 }
