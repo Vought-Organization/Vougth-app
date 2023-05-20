126b73e1d12c86b1b00aac2e6992b4fa8b9d119d
Adalberto Nascimento <nascimentoadalberto678@gmail.com>
2023-05-20 20:41:01 -0300

    feat (security): início da configuração de segurança do android

--- app/src/main/java/com/example/vought/rest/Api.kt
+++ app/src/main/java/com/example/vought/rest/Api.kt
@@ -1,14 +1,30 @@
 package com.example.vought.rest
 
+import okhttp3.Credentials
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object Api {
-    var BASE_URL = "https://voughtback.duckdns.org/"
+    var BASE_URL = "http://192.168.0.107:8080/"
+
+    // configuração de acesso para o Spring Security do backend
+    val httpClient = OkHttpClient.Builder()
+        .addInterceptor { chain ->
+            val original = chain.request()
+            val requestBuilder = original.newBuilder()
+                .header("Authorization", Credentials.basic("admin", "admin"))
+            val request = requestBuilder.build()
+            chain.proceed(request)
+        }
+        .build()
+
 
     private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
-        .baseUrl(BASE_URL).build()
+        .client(httpClient)
+        .build()
 
     fun <T> createService(serviceClass:Class<T>):T {
         return retrofit.create(serviceClass)
