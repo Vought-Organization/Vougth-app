2e7161b345314da5cd7c5ac63ac57b054d96ecda
Rafael-Araujo03 <rafael.araujo@bandtec.com.br>
2023-05-22 21:08:31 -0300

    Cards tela Splash evento

--- app/src/main/java/com/example/vought/home/NewEventFragment.kt
+++ app/src/main/java/com/example/vought/home/NewEventFragment.kt
@@ -1,5 +1,6 @@
 package com.example.vought.home
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,7 +11,10 @@ import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.example.vought.R
 import com.example.vought.databinding.FragmentAddEventBinding
+import com.example.vought.databinding.FragmentHomeBinding
+import com.example.vought.eventos.RegisterEventActivity
 import com.example.vought.model.Event
+import com.example.vought.register.RegisterActivity
 import com.example.vought.rest.Api
 import com.example.vought.rest.RetrofitService
 import retrofit2.Call
@@ -20,5 +24,22 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class NewEventFragment : Fragment(R.layout.fragment_add_event) {
+    private lateinit var binding: FragmentAddEventBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAddEventBinding.inflate(inflater)
+       return binding.root
+    }
 
+    private fun setupListeners() {
+        binding.apply {
+            cardCima.setOnClickListener {
+                val intent = Intent(requireContext(), RegisterEventActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
 }
