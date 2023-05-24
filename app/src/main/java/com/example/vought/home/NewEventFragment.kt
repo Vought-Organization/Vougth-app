3e39d48b8a3e7640a1b063770769c1adba9ddafd
guizin9 <guilherme.novais@bandtec.com.br>
2023-05-24 20:27:45 -0300

    refatorando menu

--- app/src/main/java/com/example/vought/home/NewEventFragment.kt
+++ app/src/main/java/com/example/vought/home/NewEventFragment.kt
@@ -6,22 +6,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.example.vought.R
 import com.example.vought.databinding.FragmentAddEventBinding
-import com.example.vought.databinding.FragmentHomeBinding
 import com.example.vought.eventos.RegisterEventActivity
-import com.example.vought.model.Event
-import com.example.vought.register.RegisterActivity
-import com.example.vought.rest.Api
-import com.example.vought.rest.RetrofitService
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import java.text.SimpleDateFormat
-import java.util.*
 
 class NewEventFragment : Fragment(R.layout.fragment_add_event) {
     private lateinit var binding: FragmentAddEventBinding
