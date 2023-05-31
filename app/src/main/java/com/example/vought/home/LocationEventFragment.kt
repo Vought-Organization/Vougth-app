a7d3824a7abb132d0d9bd7da20651d4f20b8666f
guizin9 <guilherme.novais@bandtec.com.br>
2023-05-31 15:48:03 -0300

    Eventos no mapa de forma dinâmica

--- app/src/main/java/com/example/vought/home/LocationEventFragment.kt
+++ app/src/main/java/com/example/vought/home/LocationEventFragment.kt
@@ -6,14 +6,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.vought.R
 import com.example.vought.home.adapter.EventAdpter
 import com.example.vought.model.DataSource
 import com.example.vought.model.Event
+import com.example.vought.rest.Api
+import com.example.vought.rest.RetrofitService
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LocationEventFragment : Fragment() {
     private var items: List<Event> = ArrayList()
@@ -39,9 +45,34 @@ class LocationEventFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync { googleMap ->
+            fetchEventsAndAddMarkers(googleMap)
             addMarkers(googleMap)
         }
     }
+
+    private fun fetchEventsAndAddMarkers(googleMap: GoogleMap) {
+        val service = Api.createService(RetrofitService::class.java)
+        val request = service.getEvent()
+
+        request.enqueue(object : Callback<List<Event>> {
+            override fun onResponse(call: Call<List<Event>>, response: Response<List<Event>>) {
+                if (response.isSuccessful) {
+                    val events = response.body()
+                    if (events != null) {
+                        setDataSet(events)
+                        addMarkers(googleMap)
+                    }
+                } else {
+                    Toast.makeText(context, "Evento não encontrado", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<List<Event>>, t: Throwable) {
+                // Handle network error
+            }
+        })
+    }
+
     fun setDataSet(events: List<Event>){
         this.items = events
     }
--- app/src/main/java/com/example/vought/rest/Api.kt
+++ app/src/main/java/com/example/vought/rest/Api.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object Api {
-    var BASE_URL = "https://voughtback.duckdns.org/"
+    var BASE_URL = "http://54.159.231.168:30080"
 
     private val retrofit: Retrofit = Retrofit.Builder()
         .addConverterFactory(GsonConverterFactory.create())
